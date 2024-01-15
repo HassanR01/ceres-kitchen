7e67456a288fd333a765edcc51e0938794bad6be
hassanR01 <hassanrageh.236@gmail.com>
2024-01-16 00:34:28 +0200

    test

--- src/app/page.js
+++ src/app/page.js
@@ -91,58 +91,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* testimonials Section */}
-      <section id="testimonials">
-        <h2>Testimonials</h2>
-        <div className={"testimonialContainer"}>
-          <div className={"testimonial"}>
-            <div className="image">
-              <Image src={'/user.png'} width={80} height={80} alt="Ahmed Ashraf Marketing" />
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aperiam laborum praesentium dicta explicabo! Accusantium cumque autem architecto similique enim vero, illum esse, qui eius aspernatur, magni repudiandae blanditiis impedit.</p>
-            <h5>Ahmed Ashraf</h5>
-          </div>
-          <div className={"testimonial"}>
-            <div className="image">
-              <Image src={'/user.png'} width={80} height={80} alt="Ahmed Ashraf Marketing" />
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aperiam laborum praesentium dicta explicabo! Accusantium cumque autem architecto similique enim vero, illum esse, qui eius aspernatur, magni repudiandae blanditiis impedit.</p>
-            <h5>Ahmed Ashraf</h5>
-          </div>
-        </div>
-      </section>
-
-      {/* Informaiton Section */}
-      <section id="info" style={{
-        backgroundImage: `url(${Interior.src})`,
-        backgroundSize: 'cover',
-      }}>
-        <div className={'content'}>
-          <div className={'brandinfo'}>
-            <Image src={'/ckwhite.svg'} width={120} height={120} alt="Ceres Kitchen Logo" />
-            <p>Food For The Body Is Not Enough, There Must Be Food For The Soul</p>
-          </div>
-          <div className={'mainmenu'}>
-            <h6>Main Menu</h6>
-            <Link href={'/about'}>About</Link>
-            <Link href={'/menu'}>Menus</Link>
-            <Link href={'/offer'}>Offers</Link>
-            <Link href={'/articals'}>Articals</Link>
-          </div>
-          <div className={'info'}>
-            <h6>Informaiton</h6>
-            <Link href={'/contact'}>Contact</Link>
-            <Link href={`/basket/${session?.user?.email}`}>Your Orders</Link>
-            <Link href={'/order_track'}>Order Track</Link>
-            <Link href={`/profile/${session?.user?.email}`}>profile</Link>
-          </div>
-          <div className={'location'}>
-            <h6>Location</h6>
-            <p>Alexandria - El Ibrahemia</p>
-            <p>10:00 AM - 11:00 PM</p>
-          </div>
-        </div>
-      </section>
     </>
   )
 }
