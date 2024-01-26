29b6cf2a2be9af38556faa0a21dd78eeda50a363
hassanR01 <hassanrageh.236@gmail.com>
2024-01-26 21:53:26 +0200

    last edits

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,7 +9,7 @@ import { signIn } from 'next-auth/react';
 export default function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
+
     const handleLogin = async (e) => {
         e.preventDefault();
 
@@ -19,7 +19,7 @@ export default function LoginForm() {
         // } else {
         //     await connectMongoDB();
         //     const userExist = await User.findOne({ email })
-            
+
         //     if (!userExist) {
         //         const res = await fetch('/api/users', {
         //             method: "POST",
@@ -40,7 +40,7 @@ export default function LoginForm() {
         //     }
 
         // }
-        
+
         console.log('Email:', email);
         console.log('Password:', password);
 
@@ -52,13 +52,8 @@ export default function LoginForm() {
     return (
         <>
             <form onSubmit={handleLogin}>
-                <label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Your Email' />
-                </label>
-                <br />
-                <label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Your Password' />
-                </label>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Your Email' />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Your Password' />
                 <br />
                 <button type="submit">Submit</button>
             </form>
--- src/components/buttons/LogOut.js
+++ src/components/buttons/LogOut.js
@@ -1,11 +1,21 @@
 'use client'
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function LogOut() {
-  return (
+  const { status, data: session } = useSession()
+  if (status === 'authenticated') {
+
+    return (
       <>
-          <Link href={'/'} onClick={() => confirm('Do you Want to Sign out ?') ? signOut() : null}>Log out</Link>
+        <Link href={'/'} onClick={() => confirm('Do you Want to Sign out ?') ? signOut() : null}>Log out</Link>
       </>
-  )
+    )
+  } else {
+    return (
+      <>
+      
+      </>
+    )
+  }
 }
